bd1bc86a4defa573573ae769d20690a04b17d901
byteihq <kotov038@gmail.com>
2021-10-10 19:17:12 +0300

    Initial commit

---
+++ include/NetworkCommunication.h
@@ -0,0 +1,25 @@
+// Copyright 2021 byteihq <kotov038@gmail.com>
+
+
+#ifndef NETWORKCOMMUNICATION_NETWORKCOMMUNICATION_H
+#define NETWORKCOMMUNICATION_NETWORKCOMMUNICATION_H
+
+enum Requests {
+    Auth,
+    GetUsers,
+    GetFile,
+    Unknown,
+};
+
+namespace Replies {
+    enum Auth {
+        Successful,
+        Unsuccessful,
+    };
+    enum Unknown {
+        Unknown
+    };
+}
+
+
+#endif //NETWORKCOMMUNICATION_NETWORKCOMMUNICATION_H
